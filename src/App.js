1614dd95db800935fe9f7383b38b2d82ff6cb9bb
Murat Kairbek <m_kairbek@outlook.com>
2025-02-20 12:45:39 +0500

    implemented sorting;

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 import './styles/App.css';
 
 
 function App() { 
   const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript 1', body: "Desc"},
-    {id: 2, title: 'JavaScript 2', body: "Desc"},
-    {id: 3, title: 'JavaScript 3', body: "Desc"},
+    {id: 1, title: 'Abba', body: "Some word"},
+    {id: 2, title: 'Python', body: "Scripting programming language"},
+    {id: 3, title: 'Java', body: "Object oriented programming language"},
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
 
   // Callback for creating the post
   const createPost = (newPost) => {
@@ -26,7 +35,21 @@ function App() {
   return (
     <div className="App">
       <PostForm create={createPost}/>
-   
+
+      <hr style={{margin: "15px 0"}}/>
+
+      <div>
+        <MySelect 
+          value={selectedSort}
+          onChange={sort => sortPosts(sort)}
+          defaultValue={"Sort by"} 
+          options= {[
+            {value: "title", name: "By name"},
+            {value: "body", name: "By description"}
+          ]}
+        />
+      </div>
+
       {posts.length !== 0
         ? <PostList remove={removePost} posts={posts} title="Posts list 1"/>
         : <h1 style={{textAlign: "center"}}>Posts are not found</h1>
