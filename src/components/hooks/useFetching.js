82d9b6b81d322c75f56c86beb629bb987ccd4833
Murat Kairbek <m_kairbek@outlook.com>
2025-02-21 20:52:38 +0500

    arguments transferred from fetching wrapper into callback;

--- src/components/hooks/useFetching.js
+++ src/components/hooks/useFetching.js
@@ -3,10 +3,10 @@ export const useFetching = (callback) => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('');
 
-    const fetching = async () => {
+    const fetching = async (...args) => {
         try {
             setLoading(true)
-            await callback()
+            await callback(...args)
         } catch (e) {
             setError(e.message)
         } finally {
